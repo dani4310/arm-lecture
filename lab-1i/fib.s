17c3f71beb3aecec85a604dabedb95ed2473399e
dani4310 <dani4310@yahoo.com.tw>
2015-03-13 18:24:34 +0800

    Simplify the code
    In my first version ,when the fibonacci function get the argument equal to 0 or 1 it will jump to .L3 or .L4 to return 0 or 1.In this version ,I let r5 contain value -1 and let r6 contain value 1 .First time r5 add r6 ,we get 0.Second time r5 add r6 we get 1. This is the same as the first and second number of Fibonacii sequence so that I don't have to determine if the argument is 0 or 1 at first.

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -13,36 +13,20 @@ fibonacci:
 	@ PROLOG
 	push {r3, r4, r5, r6, lr}
 
-	subs r4,r0,#0 ;@ R4 = R0 - 0 (update flags)
-	ble .L3 ;@ if(R0 <= 0) goto .L3 (which returns 0)
-
-	cmp r4,#1 ;@ Compare R4 wtih 1
-	beq .L4;@ If R4 == 1 goto .L4 (which returns 1)
+	add r4,r0,#1
 	
-	ldr r5,=0
-	ldr r6,=0
-	ldr r0,=1
-	sub r4,r4,#1
+	ldr r5,=-1
+	ldr r6,=1
 	
 .ITER:	
-	mov r5,r6;
-	mov r6,r0;
-	add r0,r5,r6;
-	
-	sub r4,r4,#1;
-	cmp r4,#0;
-	bne .ITER;
-	
+	add r0,r5,r6
+	mov r5,r6
+	mov r6,r0
+
+	subs r4,r4,#1
+	bne .ITER@檢查z flag=0
 	pop {r3, r4, r5, r6, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, r6, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, r6, pc}		@ EPILOG
-
 	.size fibonacci, .-fibonacci
 	.end
