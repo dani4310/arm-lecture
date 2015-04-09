7da3107ba315a2d264f42221350a69e2a0175aa7
dani4310 <dani4310@yahoo.com.tw>
2015-04-10 03:51:51 +0800

    Using less instrucion to achieve the same function.

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -11,38 +11,23 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
+	push {r4, r5, lr}
 
-	subs r4,r0,#0; @ R4 = R0 - 0 (update flags)
-	itt le ;@ if(R0 <= 0) 
-	movle r0,#0
-	pople {r3,r4,r5,pc}
+	subs r4,r0,#1
+	it le;@ If R4 <= 1 
+	pople {r4,r5,pc}
 
-
-	cmp r4,#1; @ Compare R4 wtih 1
-	itt eq;@ If R4 == 1 
-	moveq r0, #1
-	popeq {r3,r4,r5,pc}
-
-	sub r0,r4,#1; @ R0 = R4 - 1
-	bl fibonacci ;@ Recursive call to fibonacci with R4 - 1 as parameter
+	mov r0,r4; @ R0 = R4
+	bl fibonacci ;@ Recursive call to fibonacci with R4 as parameter
 
 	mov r5,r0 ;@ R5 = R0
-	sub r0,r4,#2; @ R0 = R4 - 2
-	bl fibonacci;@ Recursive call to fibonacci with R4 - 2 as parameter
+	sub r0,r4,#1; @ R0 = R4 - 
+	bl fibonacci;@ Recursive call to fibonacci with R4 -1 as parameter
 
 	adds r0,r5,r0; @ R0 = R5 + R0 (update flags)
 
-	pop {r3, r4, r5, pc}		@EPILOG
+	pop {r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
-@.L3:
-@	mov r0, #0			@ R0 = 0
-@	pop {r3, r4, r5, pc}		@ EPILOG
-@
-@.L4:
-@	mov r0, #1			@ R0 = 1
-@	pop {r3, r4, r5, pc}		@ EPILOG
-
 	.size fibonacci, .-fibonacci
 	.end
