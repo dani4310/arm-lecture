343bf9ac1717787c5c3ad059f96aa89f2d7ab62f
dani4310 <dani4310@yahoo.com.tw>
2015-04-09 21:41:18 +0800

    Only print fibonacci function executing time

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -7,11 +7,22 @@ int main(int argc, char **argv)
   int number=0;
   int result=0;
   clock_t t1,t2;
+//  FILE *fptr;
+//  fptr = fopen("recursive","w");
+//  char strbuf[10];
+
+
 
   scanf("%d",&number);
   t1=clock();
   result = fibonacci(number);
   t2=clock();   
-  printf("The fibonacci sequence at %d is: %d\nIt cost %lf seconds and %d cycles.\n\n", number, result,(t2-t1)/(double)(CLOCKS_PER_SEC),(int)(t2-t1));
+  printf("%d %lf\n", number,(t2-t1)/(double)(CLOCKS_PER_SEC));
+//  printf("The fibonacci sequence at %d is: %d\nIt cost %lf seconds and %d cycles.\n\n", number, result,(t2-t1)/(double)(CLOCKS_PER_SEC),(int)(t2-t1));
+
+//  sprintf(strbuf,"%d %lf",number,  result,(t2-t1)/(double)(CLOCKS_PER_SEC));
+//  fwrite(strbuf,1,sizeof(strbuf),fptr);
+//  fflush(fptr);
+//  fclose(fptr);
 }
 
